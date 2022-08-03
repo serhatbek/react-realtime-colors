5e44fd3d0d3130e2fecb575204af6ac53e975792
serhatbek <serhat.bekk@gmail.com>
2022-08-03 10:29:43 +0300

    realtime-colors first commit -m

---
+++ src/components/Palatte.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Palatte = () => {
+  return (
+    <div className='palatte'>
+      <input type='color' />
+      <button>Pick A Clolor</button>
+    </div>
+  );
+};
+
+export default Palatte;
